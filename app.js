0f08e5bdd540e6799755ebf4287a4d2f8b35a985
bettyChen <s9654025@gmail.com>
2019-06-26 07:40:53 +0800

    Add user login feature using passport and express-session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 const app = express()
 const port = 3000
 
@@ -25,6 +27,19 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('mongodb connected!')
+})
+app.use(session({
+  secret: 'dijwfjsogioiswg',
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./config/passport')(passport)
+
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
 })
 
 const Todo = require('./models/todo')
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  res.send('login/ submit')
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  })(req, res, next)
 })
 
 router.get('/register', (req, res) => {
